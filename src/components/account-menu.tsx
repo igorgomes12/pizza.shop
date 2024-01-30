4bd133edadc1af32f9ff61240d140386c762e5ad
Igor Gomes <igor.anjos@twed.com.br>
2024-01-30 20:59:28 -0300

    menu lateral

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -1,7 +1,14 @@
-import { ChevronDown } from 'lucide-react'
+import { Building, ChevronDown, LogOut } from 'lucide-react'
 
 import { Button } from './ui/button'
-import { DropdownMenu, DropdownMenuTrigger } from './ui/dropdown-menu'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu'
 
 export function AccountMenu() {
   return (
@@ -15,6 +22,23 @@ export function AccountMenu() {
           <ChevronDown className="h-4 w-4" />
         </Button>
       </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="w-56">
+        <DropdownMenuLabel className="flex flex-col">
+          <span>Igor Gomes</span>
+          <span className="text-xs font-normal text-muted-foreground">
+            igorgomesigla@gmail.com
+          </span>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <Building className="mr-2 h-4 w-4" />
+          <span>Perfil da loja</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
+          <LogOut className="mr-2 h-4 w-4" />
+          <span>Sair</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
     </DropdownMenu>
   )
 }
