55ecd9d60c97fbf68de8437a22d0ce06f9615aeb
Igor Gomes <igor.anjos@twed.com.br>
2024-01-31 00:49:07 -0300

    Criação da tabela de pedidos

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,6 +3,7 @@ import { createBrowserRouter } from 'react-router-dom'
 import { AppLayout } from './components/pages/_layouts/app'
 import { AuthLayout } from './components/pages/_layouts/auth'
 import { Dashboard } from './components/pages/app/dashboard'
+import { Orders } from './components/pages/app/orders/orders'
 import { SignIn } from './components/pages/auth/sign-in'
 import { SignUp } from './components/pages/auth/sign-up'
 
@@ -14,6 +15,10 @@ export const router = createBrowserRouter([
       {
         path: '/',
         element: <Dashboard />,
+      },
+      {
+        path: '/orders',
+        element: <Orders />,
       },
     ],
   },
