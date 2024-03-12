930867fd414e0feebcdf6c80ffdee030dbfa868c
Igor Gomes <igor.anjos@twed.com.br>
2024-03-11 22:56:22 -0300

    feat: Criação do gráfico de receita e ajuste de componentização.

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from 'react-router-dom'
 
 import { AppLayout } from './components/pages/_layouts/app'
 import { AuthLayout } from './components/pages/_layouts/auth'
-import { Dashboard } from './components/pages/app/dashboard'
+import { Dashboard } from './components/pages/app/dashboard/dashboard'
 import { Orders } from './components/pages/app/orders/orders'
 import { SignIn } from './components/pages/auth/sign-in'
 import { SignUp } from './components/pages/auth/sign-up'
